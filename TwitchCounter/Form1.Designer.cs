f74e69cef8a043581da90476f3ca3b4ec43a2db5
Zach <zachery.ca@gmail.com>
2022-06-15 13:18:43 -0500

    Add project files.

---
+++ TwitchCounter/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace TwitchCounter
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxCount = new System.Windows.Forms.TextBox();
+            this.buttonPlus = new System.Windows.Forms.Button();
+            this.buttonMinus = new System.Windows.Forms.Button();
+            this.buttonReset = new System.Windows.Forms.Button();
+            this.textBoxSet = new System.Windows.Forms.TextBox();
+            this.buttonSet = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxCount
+            // 
+            this.textBoxCount.Location = new System.Drawing.Point(12, 12);
+            this.textBoxCount.Name = "textBoxCount";
+            this.textBoxCount.ReadOnly = true;
+            this.textBoxCount.Size = new System.Drawing.Size(219, 23);
+            this.textBoxCount.TabIndex = 0;
+            // 
+            // buttonPlus
+            // 
+            this.buttonPlus.Location = new System.Drawing.Point(12, 41);
+            this.buttonPlus.Name = "buttonPlus";
+            this.buttonPlus.Size = new System.Drawing.Size(67, 43);
+            this.buttonPlus.TabIndex = 1;
+            this.buttonPlus.Text = "+1";
+            this.buttonPlus.UseVisualStyleBackColor = true;
+            this.buttonPlus.Click += new System.EventHandler(this.buttonPlus_Click);
+            // 
+            // buttonMinus
+            // 
+            this.buttonMinus.Location = new System.Drawing.Point(164, 41);
+            this.buttonMinus.Name = "buttonMinus";
+            this.buttonMinus.Size = new System.Drawing.Size(67, 43);
+            this.buttonMinus.TabIndex = 2;
+            this.buttonMinus.Text = "-1";
+            this.buttonMinus.UseVisualStyleBackColor = true;
+            this.buttonMinus.Click += new System.EventHandler(this.buttonMinus_Click);
+            // 
+            // buttonReset
+            // 
+            this.buttonReset.Location = new System.Drawing.Point(12, 90);
+            this.buttonReset.Name = "buttonReset";
+            this.buttonReset.Size = new System.Drawing.Size(219, 43);
+            this.buttonReset.TabIndex = 3;
+            this.buttonReset.Text = "Reset";
+            this.buttonReset.UseVisualStyleBackColor = true;
+            this.buttonReset.Click += new System.EventHandler(this.buttonReset_Click);
+            // 
+            // textBoxSet
+            // 
+            this.textBoxSet.Location = new System.Drawing.Point(85, 150);
+            this.textBoxSet.Name = "textBoxSet";
+            this.textBoxSet.Size = new System.Drawing.Size(146, 23);
+            this.textBoxSet.TabIndex = 4;
+            // 
+            // buttonSet
+            // 
+            this.buttonSet.Location = new System.Drawing.Point(12, 139);
+            this.buttonSet.Name = "buttonSet";
+            this.buttonSet.Size = new System.Drawing.Size(67, 43);
+            this.buttonSet.TabIndex = 5;
+            this.buttonSet.Text = "Set to...";
+            this.buttonSet.UseVisualStyleBackColor = true;
+            this.buttonSet.Click += new System.EventHandler(this.buttonSet_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(243, 192);
+            this.Controls.Add(this.buttonSet);
+            this.Controls.Add(this.textBoxSet);
+            this.Controls.Add(this.buttonReset);
+            this.Controls.Add(this.buttonMinus);
+            this.Controls.Add(this.buttonPlus);
+            this.Controls.Add(this.textBoxCount);
+            this.Name = "Form1";
+            this.Text = "Twitch Counter";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox textBoxCount;
+        private Button buttonPlus;
+        private Button buttonMinus;
+        private Button buttonReset;
+        private TextBox textBoxSet;
+        private Button buttonSet;
+    }
+}
---
+++ TwitchCounter/Form1.cs
@@ -0,0 +1,109 @@
+using System.IO;
+namespace TwitchCounter
+{
+    public partial class Form1 : Form
+    {
+        public int intCount;
+        public int intSet;
+        public bool boolParse;
+
+        public int intTextFileCount; 
+
+        string ExeLoc = Application.ExecutablePath; //Path to the exe, INCLUDING the .exe in the path
+        string ExeDir = AppDomain.CurrentDomain.BaseDirectory;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void checkTextFile()
+        {
+            if (File.Exists("count.txt"))
+            {
+                if (File.ReadAllText(ExeDir + "\\count.txt") == "")
+                {
+                    File.WriteAllText(ExeDir + "\\count.txt", "0");
+                }
+            }
+            else
+            {
+                File.Create("count.txt");
+                MessageBox.Show("Thanks for using Twitch Counter! A file named count.txt was created and placed in the same directory as the exe file for this application. This .txt file contains the current number counted and can be used within StreamLabs or any streaming software that allows you to display the contents of a text file on-stream.", "First Launch");
+            }
+
+            bool parse = int.TryParse(File.ReadAllText(ExeDir + "\\count.txt"), out intTextFileCount);
+            if (parse == true)
+            {
+                intCount = intTextFileCount;
+                File.WriteAllText(ExeDir + "\\count.txt", intCount.ToString());
+            }
+            else
+            {
+                MessageBox.Show("Not able to parse the contents of count.txt to an integer, has the file been modified?", "Error");
+
+            }
+        }
+
+        private void updateCount()
+        {
+            textBoxCount.Text = intCount.ToString("N0");
+
+            File.WriteAllText(ExeDir + "\\count.txt", intCount.ToString());
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.MinimumSize = this.Size;
+            this.MaximumSize = this.Size;
+
+            checkTextFile();
+
+            textBoxCount.Text = File.ReadAllText(ExeDir + "\\count.txt");
+
+            updateCount();
+        }
+
+        private void buttonPlus_Click(object sender, EventArgs e)
+        {
+            intCount += 1;
+            updateCount();
+        }
+
+        private void buttonMinus_Click(object sender, EventArgs e)
+        {
+            intCount += -1;
+            updateCount();
+        }
+
+        private void buttonReset_Click(object sender, EventArgs e)
+        {
+            DialogResult dialogResult = MessageBox.Show("Reset counter to zero?", "Reset?", MessageBoxButtons.YesNo);
+            if (dialogResult == DialogResult.Yes)
+            {
+                intCount = 0;
+                updateCount();
+            }
+        }
+
+        private void buttonSet_Click(object sender, EventArgs e)
+        {
+            boolParse = int.TryParse(textBoxSet.Text, out intSet);
+            if (boolParse == true)
+            {
+                DialogResult dialogResult = MessageBox.Show("Set counter to " + intSet.ToString() + "?", "Set?", MessageBoxButtons.YesNo);
+                if (dialogResult == DialogResult.Yes)
+                {
+                    intCount = intSet;
+                    updateCount();
+                    textBoxSet.Text = "";
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please ensure the value in the text box next to the set button is a valid integer.", "Error");
+                textBoxSet.Text = "";
+            }
+        }
+    }
+}
